e209aeeae4eb9155daed395e68e9e3fe51b67ecd
Brandon <brandonperez@brandons-mbp.lan>
2022-06-16 11:36:43 -0400

    added login functionality

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -6,13 +6,13 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect('/');
-//         return;
-//     }
-//     console.log(req.session);
-//     res.render('login');
-// });
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    console.log(req.session);
+    res.render('login');
+});
 
 module.exports = router;
