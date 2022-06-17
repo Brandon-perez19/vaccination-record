a7b18377d3161f554f35e2f48e5ddd4b8ef8f055
Brandon <brandonperez@brandons-mbp.lan>
2022-06-16 22:06:15 -0400

    changed connection.js to allow heroku to make changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
-// import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize");
 
+//import the sequelize constructor from the library
+let sequelize;
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('vaccination_record_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 
-module.exports = sequelize;
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    
+    });
+}
